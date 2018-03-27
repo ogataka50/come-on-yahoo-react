d32d2a2a805eb1ad5696e96ede4cc3afca695fa7
ogataka50 <ogataka50@gmail.com>
2018-03-28 00:27:53 +0900

    update

---
+++ src/components/Nav.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {Link} from 'react-router-dom';
+
+export default function Nav({categories}) {
+    const to = category => (
+        category.id === '1' ? '/all' : `/category/${category.id}`
+    );
+    return (
+        <ul>
+            {/* props. categories から リンク 一覧 を 生成 */}
+            {categories.map(category => (
+                <li key={`nav-item-${category.id}`}>
+                    <Link to={to(category)}>{category.name}</Link>
+                </li>
+            ))}
+        </ul>
+    );
+}
+
+Nav.propTypes = {
+    categories: PropTypes.arrayOf(
+        PropTypes.shape({
+            id: PropTypes.string.isRequired,
+            name: PropTypes.string.isRequired
+        })
+    ).isRequired
+};
+
